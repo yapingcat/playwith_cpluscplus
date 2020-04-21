b904898d11b6446d082b28d6f69f123561e211f9
yapingcat <caoyaping@163.com>
2020-04-22 00:50:41 +0800

    fix bug

--- src/cpp_standlibrary/algorithm.cpp
+++ src/cpp_standlibrary/algorithm.cpp
@@ -10,27 +10,27 @@ void test_all_any_none_of()
         logd("all_of the vector is small than 20");
     }
 
-    if(all_of(sequence.begin(),sequence.end(),[](const int& c){ if(c > 2) return true;}))
+    if(all_of(seqence.begin(),seqence.end(),[](const int& c){ if(c > 2) return true;}))
     {
         logd("some small than 2");
     }
 
-    if(any_of(seqence.begin(),seqence.end(),[](const int* c) { if(c < 2) return true;}))
+    if(any_of(seqence.begin(),seqence.end(),[](const int& c) { if(c < 2) return true;}))
     {
         logd("exist one small than 2");
     }
 
-    if(any_of(sequence.begin(),sequence.end(),[](const int& c){ if(c > 10) return true;}))
+    if(any_of(seqence.begin(),seqence.end(),[](const int& c){ if(c > 10) return true;}))
     {   
         logd("there is no number bigger than 10");
     }
 
-    if(none_of(sequence.begin(),sequence.end(),[](const int& c){ if(c > 20) return true;}))
+    if(none_of(seqence.begin(),seqence.end(),[](const int& c){ if(c > 20) return true;}))
     {
         logd("thers is no number bigger than 20");
     }
 
-    if(none_of(sequence.begin(),sequence.end(),[](const int&c){ if(c > 9) return true;}))
+    if(none_of(seqence.begin(),seqence.end(),[](const int&c){ if(c > 9) return true;}))
     {
         logd("there is one bigger than 9");
     }
