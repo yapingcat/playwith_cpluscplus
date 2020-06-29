98e16eae41f3fd222c22eb550d5e431906e1365a
yapingcat <caoyaping@163.com>
2020-06-29 23:30:20 +0800

    fix string util some bug

--- src/StringUtil.cpp
+++ src/StringUtil.cpp
@@ -15,6 +15,12 @@ void splitString(const std::string& str,char c,std::function<void(const char*,si
             pre = it + 1;
         }
     }
+    if(pre != str.end())
+    {
+        auto distanceFromBegin = std::distance(str.begin(),pre);
+        auto len = std::distance(pre,str.end());
+        func(str.c_str() + distanceFromBegin,len);
+    }
 }
 
 void splitString(const std::string& str,const std::vector<char>& vc,std::function<void(const char*,size_t)> func)
@@ -30,6 +36,12 @@ void splitString(const std::string& str,const std::vector<char>& vc,std::functio
                 func(str.c_str() + distanceFromBegin,len);
             pre = it + 1;
         }
+    }
+    if(pre != str.end())
+    {
+        auto distanceFromBegin = std::distance(str.begin(),pre);
+        auto len = std::distance(pre,str.end());
+        func(str.c_str() + distanceFromBegin,len);
     }
 }
 
@@ -52,6 +64,41 @@ std::vector<std::string> splitString(const std::string& str,const std::vector<ch
 }
 
 
+#ifdef STRINGUTIL_TEST
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::string test{"zs_cyp_love_forever"};
+    splitString(test,'_',[](const char* s,size_t len){
+                string ret(s,len);
+                std::cout<<ret<<" "<<len<<endl;
+            });
+    
+    splitString(test,'r',[](const char* s,size_t len){
+                string ret(s,len);
+                std::cout<<ret<<" "<<len<<endl;
+            });
+
+    splitString(test,{'r','_'},[](const char* s,size_t len){
+                string ret(s,len);
+                std::cout<<ret<<" "<<len<<endl;
+            });
+    
+    auto ret = splitString(test,'_');
+    for(auto&& s : ret)
+        cout<<s<<endl;
+
+    auto ret2 = splitString(test,{'r','_'});
+    for(auto&& s: ret2)
+        cout<<s<<endl;
+    return 0;
+}
+
+
+
+#endif
 
 
 
