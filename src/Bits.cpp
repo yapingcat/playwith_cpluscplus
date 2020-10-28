9ae9ab9d23ccc6226e4bc21675ace9933babf98c
yapingcat <caoyaping@163.com>
2020-10-28 23:36:49 +0800

    fix bit bug

--- src/Bits.cpp
+++ src/Bits.cpp
@@ -1,19 +1,40 @@
 #include "Bits.h"
 #include <cassert>
 
-int get_bits_of_byte(char* byte,int start_offset,int len)
+int get_bits_of_byte(unsigned char* byte,int start_offset,int len)
 {   
     assert(len <= 8 && start_offset < 8 && len + start_offset <= 8);
-    return (*byte << start_offset ) >> (start_offset + len);
+    return ((unsigned char)(*byte << start_offset) ) >> (8 - len);
 }
 
-void set_bits_of_byte(char* byte,int start_offset,int len,unsigned char v)
+void set_bits_of_byte(unsigned char* byte,int start_offset,int len,unsigned char v)
 {
     assert(len <= 8 && start_offset < 8 && len + start_offset <= 8);
     unsigned char c = 0xff;
-    unsigned char d = *byte & ~(c << start_offset) >> (start_offset + len);
-    unsigned char e = (v << start_offset ) >> (start_offset + len) << ( 8 - start_offset -  len);
-    *byte = d & e;
+    unsigned char d = *byte & ~((unsigned char)(c << start_offset) >> (8 - len) <<(8 - len - start_offset));
+    unsigned char e = v << (8 - start_offset - len);
+    *byte = ((unsigned char)(*byte & d)) | e;
 }
+
+
+#ifdef BIT_TEST
+#include <cstdio>
+#include <cstdint>
+
+int main()
+{
+    unsigned char c = 0x78;
+    auto d = get_bits_of_byte(&c,2,2);
+    printf("%x\n",c);
+    set_bits_of_byte(&c,1,3,5);
+    printf("%x\n",c);
+    set_bits_of_byte(&c,1,3,3);
+    printf("%x\n",c);
+}
+
+
+
+
+#endif
 
 
--- src/Bits.h
+++ src/Bits.h
@@ -1,8 +1,8 @@
 #ifndef BITS_H
 #define BITS_H
 
-int get_bits_of_byte(char* byte,int start_offset,int len);
-void set_bits_of_byte(char* byte,int start_offset,int len,unsigned char v);
+int get_bits_of_byte(unsigned char* byte,int start_offset,int len);
+void set_bits_of_byte(unsigned char* byte,int start_offset,int len,unsigned char v);
 
 
 
