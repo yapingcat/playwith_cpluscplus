3b0f1615bd7b970f127f311380ad291ee23307b9
yapingcat <caoyaping@163.com>
2021-01-27 23:38:11 +0800

    add rtpsender

---
+++ src/rtp/rtpsender.cpp
@@ -0,0 +1,55 @@
+//
+// blocking_udp_echo_client.cpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2019 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <thread>
+#include "asio.hpp"
+#include "RtpPacket.h"
+
+using asio::ip::udp;
+
+enum { max_length = 1024 };
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 3)
+    {
+      std::cerr << "Usage: rtpsender <host> <port>\n";
+      return 1;
+    }
+
+    asio::io_context io_context;
+
+    udp::socket s(io_context, udp::endpoint(udp::v4(), 0));
+
+    udp::resolver resolver(io_context);
+    udp::resolver::results_type endpoints =
+      resolver.resolve(udp::v4(), argv[1], argv[2]);
+    
+    for(;;)
+    {
+        RtpPacket pkg;
+        pkg.version = 2;
+        pkg.padd
+        s.send_to(asio::buffer(request, request_length), *endpoints.begin());
+    }
+
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
+}
